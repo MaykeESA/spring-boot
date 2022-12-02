4644d7074cc4f9a893acb6415403cdaf95ddab29
Mayke Erick <maykeesa.contato@gmail.com>
2022-12-02 07:05:43 -0300

    Modificação no ./topicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,23 +3,23 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> listar() {
+	public List<TopicoDto> listar() {
 		
 		Curso curso = new Curso("Java Servlet", "Back-end");
 		Topico topico = new Topico("Error 404", "Está dando esse erro na minha aplicação!", curso);
+		TopicoDto topicoDto = new TopicoDto(topico);
 		
-		return Arrays.asList(topico);
+		return Arrays.asList(topicoDto);
 	}
 }
